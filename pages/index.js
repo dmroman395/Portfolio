4b6d906cf320f921a15777e9475be609d11d628e
David Roman <dmroman395@gmail.com>
2022-04-09 14:51:45 -0400

    Corrected tailwind config content path

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 import Image from 'next/image'
 import Header from '../src/components/header'
-import styles from '../src/styles/Home.module.css'
 
 export default function Home() {
   return (
--- src/components/header.js
+++ src/components/header.js
@@ -3,7 +3,9 @@ import React from "react";
 function Header() {
     return(
         <header>
-            <h1>David M. Roman</h1>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
         </header>
     )
 }
