ed6a7fa392db85d99b61ee2160ad9db5cbfd635a
David Roman <dmroman395@gmail.com>
2022-04-18 00:20:01 -0400

    Added backdrop, need to finish animation

--- src/components/hero.js
+++ src/components/hero.js
@@ -1,17 +1,19 @@
 import React from "react";
 import Header from "./header";
+import BackDrop from "./backdrop";
 
 
 function Hero() {
     return (
-        <>
+        <div className="h-screen flex flex-col justify-center relative">
+            <BackDrop/>
             <Header/>
-            <div className="mx-10 my-24">
+            <div className="mx-10 my-24 md:mx-20 lg:px-40 lg:mx-0 z-10">
                 <span className="text-sm font-medium bg-green-300 rounded py-1 px-3">Front-End Developer</span>
                 <h1 className="text-xl text-slate-100 my-6">I recognize that life is a dream, and I dream lucid.</h1>
                 <p className="text-sm text-zinc-400">I build dope shit.</p>
             </div>
-        </>
+        </div>
     )
 }
 
