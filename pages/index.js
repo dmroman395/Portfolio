4d6f932a89ea9de4fe0dc9964c860dc926d4770b
David Roman <dmroman395@gmail.com>
2022-04-24 14:01:56 -0400

    Added Open Graph meta tags and refactored links in header

--- pages/index.js
+++ pages/index.js
@@ -8,14 +8,18 @@ import Footer from '../src/components/footer';
 export default function Home() {
   return (
     <>
-    <Head>
-      <title>David M. Roman | Portfolio</title>
-      <meta name='description' content="I am David Miguel Roman, a New York based, self-taught front-end developer with a strong desire to learn and grow as a developer."/>
-      <link rel="canonical" href="https://davidmiguel.io" />
-      <link rel="preconnect" href="https://fonts.googleapis.com"/>
-      <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
-      <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&family=Roboto+Mono:wght@300;400&display=swap" rel="stylesheet"/>
-    </Head>
+      <Head>
+        <title>David M. Roman | Portfolio</title>
+        <meta name='description' content="I'm David Miguel Roman, a New York based, self-taught, front-end developer with a strong desire to learn and grow."/>
+        <meta name="robots" content="all" />
+        <meta property="og:title" content="David M. Roman | Portfolio" />
+        <meta property="og:description" content="I'm David Miguel Roman, a New York based, self-taught, front-end developer with a strong desire to learn and grow."/>
+        <meta property="og:image" content="https://davidmiguel.io/images/portfolio.jpeg"/>
+        <link rel="canonical" href="https://davidmiguel.io" />
+        <link rel="preconnect" href="https://fonts.googleapis.com"/>
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin/>
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;600;700&family=Roboto+Mono:wght@300;400&display=swap" rel="stylesheet"/>
+      </Head>
       <div className='wrapper'>
         <Hero/>
         <MoreInfo/>
--- src/components/header.js
+++ src/components/header.js
@@ -1,4 +1,5 @@
 import React, { useRef, useEffect } from "react";
+import Link from "next/link";
 
 function Header() {
     const header = useRef();
@@ -23,9 +24,9 @@ function Header() {
         <header ref={header} className='hide transition-all ease-in-out duration-1000 py-10 px-7 z-20 text-slate-100 flex justify-between sm:p-12 md:px-20 lg:pt-16 lg:px-40 absolute top-0 w-screen'>
             <h1 className='text-lg sm:text-2xl lg:text-3xl'><span className='font-semibold'>David</span><span className='text-neutral-300'>Roman</span></h1>
             <ul className='flex text-xs font-normal sm:text-sm sm:gap-x-6 lg:gap-10 xl:gap-16 lg:text-base'>
-                <li className='listItem my-2 mx-1 text-shadow transition-colors duration-300 ease-in-out'><a href='#moreInfo'>More Info</a></li>
-                <li className='listItem my-2 mx-1 text-shadow transition-colors duration-300 ease-in-out'><a href='#projects'>Projects</a></li>
-                <li className='listItem my-2 mx-1 text-shadow transition-colors duration-300 ease-in-out'><a href='#contact'>Contact</a></li>
+                <li className='listItem my-2 mx-1 text-shadow transition-colors duration-300 ease-in-out'><Link href={'#moreInfo'}><a>More Info</a></Link></li>
+                <li className='listItem my-2 mx-1 text-shadow transition-colors duration-300 ease-in-out'><Link href={'#projects'}><a>Projects</a></Link></li>
+                <li className='listItem my-2 mx-1 text-shadow transition-colors duration-300 ease-in-out'><Link href={'#contact'}><a>Contact</a></Link></li>
             </ul>
         </header>
     )
