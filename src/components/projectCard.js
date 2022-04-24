30cd8bdf8864939ffc7e5d04735b4cb3df4c8458
David Roman <dmroman395@gmail.com>
2022-04-23 20:14:09 -0400

    Finished responsive design

--- src/components/projectCard.js
+++ src/components/projectCard.js
@@ -34,23 +34,24 @@ function ProjectCard({title, img, link, code, tech, id, side}) {
         }
     })
     return (
-        <div className={`relative md:sticky transition-all ease-in-out px-8 py-16 divide-zinc-50 h-screen flex flex-grow top-0 md:w-[50%]`} id={id}>
-            <div ref={project} className={`${side === 'left' ? 'hideProjectCardLeft' : 'hideProjectCardRight'} flex flex-col gap-y-5 w-full h-fit self-center`}>
-                <h1 className="text-3xl text-center text-slate-100">{title}</h1>
+        <div className={`relative md:sticky transition-all ease-in-out px-8 py-16 divide-zinc-50 h-screen flex flex-grow justify-center top-0 md:w-[50%] lg:px-16`} id={id}>
+            <div ref={project} className={`${side === 'left' ? 'hideProjectCardLeft' : 'hideProjectCardRight'} flex flex-col gap-y-5 w-full h-fit self-center sm:max-w-[450px] lg:max-w-[550px] xl:max-w-[700px]`}>
+                <h1 className="text-3xl text-center md:text-4xl lg:text-5xl xl:text-6xl text-slate-100">{title}</h1>
                 <div className="my-3">
                     <Image
                         src={img}
                         width={556}
                         height={407}
                         layout='responsive'
+                        className='rounded-lg'
                     />
                 </div>
-                <ul className="flex flex-wrap justify-center my-3 text-slate-100">
+                <ul className="flex flex-wrap justify-center my-3 text-slate-100 self-center lg:text-lg xl:text-2xl max-w-[400px] xl:max-w-[600px]">
                     {techList}
                 </ul>
                 <div className="flex justify-around">
-                    {link ? <a href={link} target='_blank' rel="noreferrer" className='underline greenText hover:cursor-pointer'>View Live</a> : null}
-                    <a href={code} target='_blank' rel="noreferrer" className='underline greenText hover:cursor-pointer'>View Source Code</a>
+                    {link ? <a href={link} target='_blank' rel="noreferrer" className='underline greenText hover:cursor-pointer lg:text-lg xl:text-2xl'>View Live</a> : null}
+                    <a href={code} target='_blank' rel="noreferrer" className='underline greenText hover:cursor-pointer lg:text-lg xl:text-2xl'>View Source Code</a>
                 </div> 
             </div>  
         </div>
--- src/components/projectInfo.js
+++ src/components/projectInfo.js
@@ -21,30 +21,29 @@ function ProjectInfo({overview, inspiration, challenges, solutions, lessons, sid
         },
         {
             threshold: 0.4
-        }
-        )
+        })
         observer.observe(info.current)
     },[])
 
     const chalList = challenges.map((challenge, i) => {
-        return <li key={i} className="md:leading-7">{challenge}</li>
+        return <li key={i} className="tracking-wider md:leading-7">{challenge}</li>
     })
 
     const solList = solutions.map((solution, i) => {
-        return <li key={i} className="md:leading-7">{solution}</li>
+        return <li key={i} className="tracking-wioder md:leading-7">{solution}</li>
     })
     return (
         <div ref={info} className={`${side === 'left' ? 'hideProjectInfoLeft' : 'hideProjectInfoRight'} p-10 mb-12 mt-5 md:w-[50%] md:p-16`}>
-            <h1 className="text-slate-100 my-5 text-xl font-semibold md:text-3xl">Overview</h1>
-                <p className="text-zinc-400 text-base tracking-wide leading-6 md:leading-7 md:my-10">{overview}</p>
-            <h1 className="text-slate-100 my-5 text-xl font-semibold md:text-3xl">Inspiration</h1>
-                <p className="text-zinc-400 text-base tracking-wider leading-6 md:leading-7 md:my-10">{inspiration}</p>
-            <h1 className="text-slate-100 my-5 text-xl font-semibold md:text-3xl">Challenges</h1>
-                <ol className="text-zinc-400 pl-4 list-decimal ml-4 md:my-10">{chalList}</ol>
-            <h1 className="text-slate-100 my-5 text-xl font-semibold md:text-3xl">Solutions</h1>
-                <ol className="text-zinc-400 pl-4 list-decimal ml-4 md:my-10">{solList}</ol>
-            <h1 className="text-slate-100 my-5 text-xl font-semibold md:text-3xl">What I learned</h1>
-                <p className="text-zinc-400 text-base tracking-wide leading-6 md:leading-7 md:my-10">{lessons}</p>
+            <h1 className="text-slate-100 my-5 text-xl font-semibold md:text-3xl lg:text-4xl xl:text-5xl">Overview</h1>
+                <p className="text-zinc-400 text-base tracking-wider leading-6 md:leading-7 md:my-10 lg:text-lg xl:text-2xl xl:mb-12">{overview}</p>
+            <h1 className="text-slate-100 my-5 text-xl font-semibold md:text-3xl lg:text-4xl xl:text-5xl">Inspiration</h1>
+                <p className="text-zinc-400 text-base tracking-wider leading-6 md:leading-7 md:my-10 xl:text-2xl xl:mb-12">{inspiration}</p>
+            <h1 className="text-slate-100 my-5 text-xl font-semibold md:text-3xl lg:text-4xl xl:text-5xl">Challenges</h1>
+                <ol className="text-zinc-400 pl-4 xl:pl-9 list-decimal ml-4 md:my-10 xl:mb-12 xl:text-2xl">{chalList}</ol>
+            <h1 className="text-slate-100 my-5 text-xl font-semibold md:text-3xl lg:text-4xl xl:text-5xl">Solutions</h1>
+                <ol className="text-zinc-400 pl-4 xl:pl-9 list-decimal ml-4 md:my-10 xl:text-2xl xl:mb-12">{solList}</ol>
+            <h1 className="text-slate-100 my-5 text-xl font-semibold md:text-3xl lg:text-4xl xl:text-5xl">What I learned</h1>
+                <p className="text-zinc-400 text-base tracking-wider leading-6 md:leading-7 xl:text-2xl md:my-10">{lessons}</p>
         </div>
     )
 }
