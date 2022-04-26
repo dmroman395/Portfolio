fd11e5559f608270cda3550e69df56c66311c1af
David Roman <dmroman395@gmail.com>
2022-04-26 01:20:19 -0400

    Added some changes to max width of large text in hero

--- src/components/hero.js
+++ src/components/hero.js
@@ -29,7 +29,7 @@ function Hero() {
             <Header/>
             <div ref={hero} className="hide transition-all ease-in-out duration-1000 h-screen w-screen flex flex-col gap-5 justify-center items-end px-10 py-24 md:px-20 lg:px-40 z-10">
                 <span className="text-sm max-w-fit font-medium greenBg rounded py-1 px-3 sm:text-base md:font-normal">Front-End Developer</span>
-                <h1 className="text-xl text-right tracking-wide leading-loose text-slate-100 max-w-[65%] my-6 sm:text-2xl md:text-3xl lg:text-4xl lg:my-10 xl:text-6xl">Passionate problem solver, aspiring to be more than just average.</h1>
+                <h1 className="text-xl text-right tracking-wide leading-loose text-slate-100 max-w-[80%] xl:max-w-[65%] my-6 sm:text-2xl md:text-3xl lg:text-4xl lg:my-10 xl:text-6xl">Passionate problem solver, aspiring to be more than just average.</h1>
                 <p className="text-sm italic text-zinc-400 sm:text-base md:text-lg">Scroll down to learn more</p>
             </div>
         </div>
